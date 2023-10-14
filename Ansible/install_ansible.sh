b00875fdf469edc71c4031aa197d23b5e8ff02d8
Michaël Bontyes <michaelbontyes@gmail.com>
2023-10-13 22:54:46 -0400

    Update install_ansible.sh

--- Ansible/install_ansible.sh
+++ Ansible/install_ansible.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Update the system
-sudo apt update
+apt update
 
 # Install software-properties-common (required for add-apt-repository)
-sudo apt install -y software-properties-common
+apt install -y software-properties-common
 
 # Add the Ansible PPA
-sudo add-apt-repository --yes --update ppa:ansible/ansible
+add-apt-repository --yes --update ppa:ansible/ansible
 
 # Install Ansible
-sudo apt install -y ansible
+apt install -y ansible
 
 # Verify the installation
 ansible --version
