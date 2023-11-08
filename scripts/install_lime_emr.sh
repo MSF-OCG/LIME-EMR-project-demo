c5f103e55a20311cf8afb4628e0f4d8b529d9209
Michaël Bontyes <michaelbontyes@gmail.com>
2023-11-08 11:33:09 +0100

    New installation script

---
+++ scripts/install_lime_emr.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+
+# Configurable variables
+REPOSITORY_URL="https://github.com/MSF-OCG/LIME-EMR-project-demo.git"
+BRANCH_NAME="main" # Default branch name
+INSTALL_DIR="$HOME/LIME"
+LOG_DIR="$HOME/logs"
+SUCCESS_LOG="$LOG_DIR/install_script_success.log"
+ERROR_LOG="$LOG_DIR/install_script_error.log"
+APP_URL="http://localhost/openmrs/login.htm" 
+CONTAINER_NAMES="openmrs-db openmrs-frontend openmrs-backend gateway"
+MAX_ATTEMPTS=5
+MAX_RETRIES=18
+COMPOSE_VERSION="2.23.0"
+
+# Ensure the log directories and files exist
+mkdir -p "$LOG_DIR"
+touch "$SUCCESS_LOG"
+touch "$ERROR_LOG"
+
+# Function to log success messages
+log_success() {
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')] Success: $1" >> "$SUCCESS_LOG"
+}
+
+# Function to log error messages
+log_error() {
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')] Error: $1" >> "$ERROR_LOG"
+}
+
+# Check prerequisites (requires manual intervention if not installed)
+check_prerequisites() {
+    for pkg in git curl vim screen gettext-base jq pwgen mlocate rsync software-properties-common apt-transport-https ca-certificates gnupg2; do
+        if ! dpkg -l "$pkg" > /dev/null 2>&1; then
+            echo "The package $pkg is missing and needs to be installed."
+            echo "Please run 'sudo apt-get install $pkg' to install it."
+            exit 1
+        fi
+    done
+    log_success "All required packages are installed."
+}
+
+# Function to install Docker Compose if not already installed
+install_docker_compose() {
+    if ! docker-compose --version > /dev/null 2>&1 || [ "$(docker-compose --version | awk '{print $3}' | cut -d ',' -f1)" != "$COMPOSE_VERSION" ]; then
+        echo "Installing Docker Compose..."
+        curl -L "https://github.com/docker/compose/releases/download/v${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+        chmod +x /usr/local/bin/docker-compose && \
+        log_success "Installed Docker Compose version $COMPOSE_VERSION" || \
+        log_error "Failed to install Docker Compose."
+    else
+        log_success "Docker Compose version $COMPOSE_VERSION is already installed."
+    fi
+}
+
+# Function to check Docker containers
+check_containers() {
+    for container in $CONTAINER_NAMES; do
+        attempt=1
+        while [ $attempt -le $MAX_ATTEMPTS ]; do
+            if docker ps --format '{{.Names}}' | grep -q "^${container}$"; then
+                log_success "Container $container is running."
+                break
+            else
+                if [ $attempt -eq $MAX_ATTEMPTS ]; then
+                    log_error "Container $container failed to run after $MAX_ATTEMPTS attempts."
+                    return 1
+                fi
+                echo "Attempt $attempt of $MAX_ATTEMPTS: Container $container is not running."
+                attempt=$((attempt+1))
+                sleep 5
+            fi
+        done
+    done
+    return 0
+}
+
+# Function to verify the application URL is available
+verify_application_url() {
+    attempt=1
+    while [ $attempt -le $MAX_RETRIES ]; do
+        http_status=$(curl -o /dev/null -s -w "%{http_code}\n" -I "$APP_URL")
+        if [ "$http_status" -eq 200 ]; then
+            log_success "The application at $APP_URL is reachable."
+            return
+        else
+            echo "Attempt $attempt of $MAX_RETRIES: The application at $APP_URL is not reachable. Status: $http_status"
+            if [ $attempt -eq $MAX_RETRIES ]; then
+                log_error "Failed to reach the application at $APP_URL after $MAX_RETRIES attempts."
+                return 1
+            fi
+            sleep 10
+            attempt=$((attempt+1))
+        fi
+    done
+}
+
+# Clone the repository into INSTALL_DIR/LIME
+clone_repository() {
+    if [ ! -d "$INSTALL_DIR" ]; then
+        git clone --single-branch --branch "$BRANCH_NAME" "$REPOSITORY_URL" "$INSTALL_DIR" && \
+        log_success "Cloned the repository into $INSTALL_DIR." || \
+        log_error "Failed to clone the repository into $INSTALL_DIR."
+    else
+        log_success "The directory $INSTALL_DIR already exists."
+    fi
+}
+
+# Main installation function
+install_application() {
+    if check_prerequisites && install_docker_compose; then
+        clone_repository
+        if check_containers; then
+            verify_application_url
+        fi
+    else
+        return 1
+    fi
+}
+
+# Start the installation process
+if install_application; then
+    log_success "Installation and verifications completed successfully."
+else
+    log_error "Installation or verification failed."
+    exit 1
+fi
