2379fcd2e6be4272700d397d4eace03a078a460d
jnsereko <nserekojowashi@gmail.com>
2024-05-17 13:58:37 +0300

    try to reduce build time by using actions/cache@v4 and restore

--- distro/e2e_test_support_files/extract_tag_numbers.sh
+++ distro/e2e_test_support_files/extract_tag_numbers.sh
@@ -10,18 +10,16 @@ get_repository_tag() {
   local ref="refs/tags/v$version"
 
   # Check if the version number contains "pre" and modify the ref to main branch
-  if [[ $version == *"pre"* ]]; then
+  if [[ $version == *"pre"* || $version == *"next"* ]]; then
     ref="main"
   fi
 
   echo "$repo_name=$ref"
 }
 
-file_path="frontend/spa-build-config.json"
+file_path="frontend/spa-assemble-config.json"
 
 # Call the function for each Repository with the app as the second argument
 get_repository_tag "$file_path" "patient_management" "@openmrs/esm-patient-registration-app" >> "$GITHUB_OUTPUT"
 get_repository_tag "$file_path" "patient_chart" "@openmrs/esm-patient-chart-app" >> "$GITHUB_OUTPUT"
 get_repository_tag "$file_path" "esm_core" "@openmrs/esm-login-app" >> "$GITHUB_OUTPUT"
-get_repository_tag "$file_path" "form_builder" "@openmrs/esm-form-builder-app" >> "$GITHUB_OUTPUT"
-get_repository_tag "$file_path" "cohort_builder" "@openmrs/esm-cohort-builder-app" >> "$GITHUB_OUTPUT"
